ef0e6e3dc8cd46056f11611f068fc930118108a6
MaxHolzmann <max@holzmann.io>
2022-10-13 13:16:48 -0500

    todo list and random num generator

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 
 // fetch meals from a JSON file here
+const randomNum = (max) => {
+  let rand = Math.random() * max;
+    return Math.floor(rand)
+}
+
 
 const generateButton = document.getElementById('genBtn');
 generateButton.addEventListener('click', (e) => {
@@ -7,28 +12,32 @@ generateButton.addEventListener('click', (e) => {
   const mealsJSON = fetch("https://maxholzmann.github.io/meal-generator/meals.json")
   .then(res=> res.json()) 
   .then(data => {
-    const foodName = data.entrees[0].name;
-    const foodType = data.entrees[0].type;
+
+    let mealNumber = randomNum(data.entrees.length)
+
+    console.log(mealNumber)
+
+    const foodName = data.entrees[mealNumber].name;
+    const foodType = data.entrees[mealNumber].type;
     console.log(foodName + " is the name of the food");
     console.log(foodType + " is the type of food!");
   });    // GENERATE A MEAL AND MANIPULATE THE DOM TO DISPLAY THE MEAL HERE!!!
 })
+/*
+
+TO-DO PSEUDO CODE: 
+
+use JS to update the dom to the meal, instead of logging to console
+
+*/
 
 
+/* ADDTIONAL TO DO (Extra features) 
 
-/* 
+add Sides
 
-psuedo code: 
+add random side button
 
+add more meals
 
-event listener listening for a click of the "generate" button - complete
-
-fetch a list of entrees and sides from a JSON file (done!)
-
-
-grab a random entree and a random side (??? how ??) something with math.floor,
-
-
-return a randomized meal to the screen through DOM maniuplation 
-
-*/ 
+*/
